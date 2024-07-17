5ae04646cd95a3fd134cd72f1f7791fa02751894
= <=>
2024-07-17 10:57:47 +0900

    feat: 논리사고 흐름 > 중첩 반복문, 중첩 조건문 줄이기

--- src/test/java/juno/Scribbling2.java
+++ src/test/java/juno/Scribbling2.java
@@ -15,11 +15,35 @@ public class Scribbling2 {
             System.out.println("hello 0");
             return ;
         }
-
         if (count < 2) {
             System.out.println("hello 1");
             return;
         }
         System.out.println("hello 2");
     }
+
+    @Test
+    void depth() {
+        int count = 3;
+
+        // 코드 30줄...
+
+        printHello(count);
+    }
+
+    @Test
+    void depth2() {
+        for (int i=0; i<30; i++) {
+            printByI(i);
+        }
+    }
+
+    private static void printByI(int i) {
+        for (int j=0; j<10; j++) {
+            if (i > 10 && j < 5) {
+                System.out.println("출력");
+            }
+        }
+    }
+
 }
